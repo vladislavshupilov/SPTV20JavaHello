ef8affcd179adaf0429fae7aa9f048afa2fdc75d
otmazka <svetlankas@hot.ee>
2021-09-28 12:37:31 +0300

    JavaHello1

---
+++ src/javahello/JavaHello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javahello;
+
+/**
+ *
+ * @author User
+ */
+public class JavaHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello!");
+    }
+    
+}
